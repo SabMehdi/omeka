5332b7f43ef5354accbd10cd5d32d91d17e8b518
saber <kadiri112@icloud.com>
2023-11-26 15:51:16 +0100

    login

--- src/Login.js
+++ src/Login.js
@@ -1,31 +1,71 @@
-// src/components/Login.js
-
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
+import { useUser } from './UserContext'; // Adjust the path as necessary
+import { useNavigate } from 'react-router-dom';
+const Login = () => {
+    const { updateCredentials } = useUser();
+    const [keyIdentity, setKeyIdentity] = useState('');
+    const [keyCredential, setKeyCredential] = useState('');
+    const [users, setUsers] = useState([]); 
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState('');
+    const navigate=useNavigate()
+    const handleLogin = async (event) => {
+        event.preventDefault();
+        setLoading(true);
+        setError('');
 
-const Login = ({ onLoginSuccess }) => {
-  const keyIdentity = 'DO5PuNIDlF7ge3dQfk6M4PnnduUHzfPV';
-  const keyCredential = 'l0klRKNKbbMPjSv9bTRLIygzfHgMc7uD';
+        try {
+            const response = await axios.get('/api/users', {
+                params: {
+                  key_identity: keyIdentity,
+                  key_credential: keyCredential
+                }
+            });
+            
+            setUsers(response.data); 
+            updateCredentials({ key_identity: keyIdentity, key_credential: keyCredential });
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    try {
-      const response = await axios.post('http://localhost/omeka-s/api/users', {
-        key_identity: keyIdentity,
-        key_credential: keyCredential
-      });
-      // Handle login success
-      onLoginSuccess(response.data);
-    } catch (error) {
-      console.error('Login failed:', error);
-    }
-  };
+            navigate('/resource');
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <button type="submit">Login</button>
-    </form>
-  );
+            
+        } catch (error) {
+            console.error('Error fetching user data:', error);
+            setError('Failed to fetch user data. Please check the credentials and try again.');
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    return (
+        <div>
+            <h2>Login to Fetch Users</h2>
+            <form onSubmit={handleLogin}>
+                <input 
+                    type="text" 
+                    placeholder="Key Identity" 
+                    value={keyIdentity} 
+                    onChange={(e) => setKeyIdentity(e.target.value)} 
+                />
+                <input 
+                    type="password" 
+                    placeholder="Key Credential" 
+                    value={keyCredential} 
+                    onChange={(e) => setKeyCredential(e.target.value)} 
+                />
+                <button type="submit" disabled={loading}>
+                    {loading ? 'Loading...' : 'Login'}
+                </button>
+            </form>
+            {error && <p style={{ color: 'red' }}>{error}</p>}      
+              {/* Display users if needed */}
+              <ul>
+              {users.map(user => (
+                <li key={user['o:id']}>{user['o:name']}</li>
+              ))}
+            </ul>   
+        </div>
+    );
 };
 
 export default Login;
